dfc05bc1bd7e4c817122d5a7acaa3a2c9ae3dbce
samlam <administration@gwngames.com>
2024-12-07 10:34:56 +0100

    fix

--- net/gwngames/pubscraper/scraper/scraper/ScimagoScraper.py
+++ net/gwngames/pubscraper/scraper/scraper/ScimagoScraper.py
@@ -92,7 +92,7 @@ class ScimagoScraper(GeneralScraper):
                     q_rank_element = cells[3].find("span", class_="q1") if len(cells) > 3 else None
                     q_rank = q_rank_element.get_text(strip=True) if q_rank_element else sjr[-2:] if sjr[-2] == 'q' else "N/A"
 
-                    match = re.search(r'year=(\d{4})', url)
+                    match = re.search(r'year=(\d{4})', link)
                     year = match.group(1) if match else 0
                     if year is 0:
                         continue
